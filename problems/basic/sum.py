ff746fa73968f360a0634434de54ead1a59982b8
krish2004-creator <vkrishnaveni299@gmail.com>
2024-10-31 22:15:48 +0530

    problems

---
+++ problems/basic/sum.py
@@ -0,0 +1,14 @@
+
+#parameters
+def sum(a,b):
+    return a+b
+
+num1 = int(input("Enter num 1:"))
+num2 = int(input("Enter num 2:"))
+
+#orguments
+result = sum(num1,num2)
+#string formating
+print(f"the sum is : {result}")
+print("the sum is : {}".format(result))
+
